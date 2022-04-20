ca652d43241733de365d4b5b7b6e6ad666517226
erik schievenin <erik.schievenin@gmail.com>
2022-04-20 18:50:11 +0200

    first milestone 1

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,92 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <FlixHeader @search="startSearch" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import FlixHeader from "./components/FlixHeader.vue";
+import axios from "axios";
 
 export default {
   name: "App",
-  components: {},
+  components: {
+    FlixHeader,
+  },
+  data() {
+    return {
+      movieFound: [],
+      seriesFound: [],
+
+      // allGeners: [],
+
+      genresFound: "",
+      apiKey: "3fcf4a781e2ec238aae1da6edb339733",
+      language: "it-IT",
+    };
+  },
+
+  methods: {
+    // Funzione di ricerca
+    startSearch(searchValue) {
+      this.getMovie(searchValue);
+      this.getTv(searchValue);
+    },
+
+    // Funzione di ricerca film
+    getMovie(searchValue) {
+      const params = {
+        params: {
+          api_key: this.apiKey,
+          query: searchValue,
+          language: this.language,
+        },
+      };
+
+      axios
+        .get("https://api.themoviedb.org/3/search/movie", params)
+        .then((response) => {
+          this.movieFound = response.data.results;
+
+          console.log(this.movieFound);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+
+    // Funzione di ricerca serie TV
+    getTv(searchValue) {
+      const params = {
+        params: {
+          api_key: this.apiKey,
+          query: searchValue,
+          language: this.language,
+        },
+      };
+
+      axios
+        .get("https://api.themoviedb.org/3/search/tv", params)
+        .then((response) => {
+          this.seriesFound = response.data.results;
+
+          console.log(this.seriesFound);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import "./assets/style/common.scss";
+
+body {
+  background-image: url("../src/assets/img/netflix-image.jpg");
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center top;
 }
 </style>
---
+++ src/components/FlixHeader.vue
@@ -0,0 +1,59 @@
+<template>
+  <header>
+    <h1 id="site-name">BOOLFLIX</h1>
+
+    <form class="searchPanel">
+      <input
+        type="text"
+        placeholder="Cerca il tuo film o la tua serie TV preferita"
+        v-model="inputSearch"
+      />
+
+      <button type="submit" @click.prevent="$emit('search', inputSearch)">
+        Cerca
+      </button>
+    </form>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "FlixHeader",
+
+  props: {
+    genre: Array,
+  },
+
+  data() {
+    return {
+      inputSearch: "",
+      targetGenre: "",
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import "../assets/style/common.scss";
+header {
+  height: 80px;
+  background-color: black;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 10px;
+
+  input {
+    width: 330px;
+  }
+
+  input,
+  button {
+    padding: 5px;
+    font-size: 15px;
+    text-align: center;
+
+    margin-left: 5px;
+  }
+}
+</style>

5b67f722d60fcaf3c66a41a6c2db59b72f67b4fb
erik schievenin <erik.schievenin@gmail.com>
2022-04-21 17:38:27 +0200

    milestone 2

---
+++ src/components/partials/FlixMovies.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+    <h1 class="main-title">FILM</h1>
+    <div class="container">
+      <ul class="gallery"></ul>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "FlixMovies",
+  props: {
+    infoMovie: Array,
+    infoGenre: Array,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped lang="scss">
+</style>
---
+++ src/components/partials/FlixSeries.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <h1 class="main-title">SERIE TV</h1>
+    <div class="container">
+      <ul class="gallery"></ul>
+    </div>
+  </section>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+</style>
